55badd3421b2b049d177f0dc47302ae52de1aeac
Tatyana <tatyanalipch@yandex.ru>
2024-06-17 00:23:52 +0300

    src/

--- src/main/java/NotFoundException.java
+++ src/main/java/NotFoundException.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class NotFoundException {
+import java.lang.module.ResolutionException;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String s) {
+        super(s);
+    }
+
+
+public void removeById(int id) throws Exception {
+
+        if (id == 0) {
+
+             throw new NotFoundException(
+
+             "Element with id: \" + id + \" not found"
+            );
+            //throw e;
+        }
+}
+
+    public Product findById(int ID) {
+
+
+        for (Product product : products) {
+            if (id == product ) {
+
+                return product;
+            }
+        }
+        return null;
+    }
 }
--- src/test/java/ShopRepositoryTest.java
+++ src/test/java/ShopRepositoryTest.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class ShopRepositoryTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShopRepositoryTest {
+    PurchaseItem item1 = new PurchaseItem(11,1, "хлеб", 20,7);
+    PurchaseItem item2 = new PurchaseItem(11,1, "хлеб", 20,7);
+    PurchaseItem item3 = new PurchaseItem(11,1, "хлеб", 20,7);
+    @Test
+
+    public void test() {
+        ShopRepository repo = new ShopRepository();
+        repo.save(item1);
+        repo.save(item2);
+        repo.save(item3);
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            repo.reemoveById(100);
+        });
+
+    }
+
 }
